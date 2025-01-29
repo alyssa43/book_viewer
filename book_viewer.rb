318a40f1368195c57fece7a2899c7d95430b8a46
Alyssa Easter <AlyssaStarley@gmail.com>
2025-01-29 11:54:09 -0800

    Added before filter and view helper

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,9 +2,22 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+
+  def in_paragraphs(text)
+    text.split("\n\n").map do |paragraph|
+      "<p>#{paragraph}</p>"
+    end.join
+  end
+  
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
 
   erb :home
 end
@@ -14,8 +27,6 @@ get "/show/:name" do
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines("data/toc.txt")
-
   number = params[:number].to_i
   chapter_name = @contents[number - 1]
   @title = "Chapter #{number}: #{chapter_name}"
